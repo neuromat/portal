f9e4e5c442d6ca4de846fbcb6dfb2360e078bcda
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-09 13:42:00 -0300

    Functional tests now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,27 +1,36 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# A neuroscience researcher discovered a new site that
-# provides a data base with neuroscience experiments.
-# She goes to checkout its home page
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-# She notices the page title and header mention
-# Neuroscience Experiments Database
-assert 'Neuroscience Experiments Database' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
-# She sees the home page have a list of experiments
-# and click one one of that to check it out
+        # A neuroscience researcher discovered a new site that
+        # provides a data base with neuroscience experiments.
+        # She goes to checkout its home page
+        self.browser.get('http://localhost:8000')
 
-# She see that there is a search box in top of page.
-# So, she types in input search box:
-# "Brachial Plexus" and wait to see the results.
+        # She notices the page title and header mention
+        # Neuroscience Experiments Database
+        set.assertIn('Neuroscience Experiments Database', self.browser.title)
+        self.fail ('Finish the test!')
 
-# Then she see that some entries returned that
-# supposely has "Brachial Plexus" string in one
-# or more fields
+    # She sees the home page have a list of experiments
+    # and click one one of that to check it out
 
-browser.quit()
+    # She see that there is a search box in top of page.
+    # So, she types in input search box:
+    # "Brachial Plexus" and wait to see the results.
 
+    # Then she see that some entries returned that
+    # supposely has "Brachial Plexus" string in one
+    # or more fields
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
