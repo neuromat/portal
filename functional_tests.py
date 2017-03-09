43f9cb17da7a6ee494c7969e1879c26c5b7152a2
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-09 13:30:08 -0300

    First functional test with User Stories

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 
 
 browser = webdriver.Firefox()
+
+# A neuroscience researcher discovered a new site that
+# provides a data base with neuroscience experiments.
+# She goes to checkout its home page
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention
+# Neuroscience Experiments Database
+assert 'Neuroscience Experiments Database' in browser.title
+
+# She sees the home page have a list of experiments
+# and click one one of that to check it out
+
+# She see that there is a search box in top of page.
+# So, she types in input search box:
+# "Brachial Plexus" and wait to see the results.
+
+# Then she see that some entries returned that
+# supposely has "Brachial Plexus" string in one
+# or more fields
+
+browser.quit()
+
