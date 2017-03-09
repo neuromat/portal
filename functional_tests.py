6a3b934097d06efdb58dab05b0982f74efe6f07c
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-09 17:01:12 -0300

    Functional test now checks we can input a term to search in data base

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,7 @@ import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -19,18 +20,37 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention
         # Neuroscience Experiments Database
         self.assertIn('Neuroscience Experiments Database', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('<h1>').text
+        self.assertIn('Neuroscience Experiments Database', header_text) 
 
         # She sees the home page have a list of experiments
         # and click one one of that to check it out
 
-        # She see that there is a search box in top of page.
-        # So, she types in input search box:
-        # "Brachial Plexus" and wait to see the results.
+        # She sees that there is a search box in top of page.
+        searchbox = self.browser.find_element_by_id('id_searchbox')
+        self.assertEqual(
+            searchbox.get_attribute('placeholder'),
+            'Enter a search text'
+        )
 
-        # Then she see that some entries returned that
-        # supposely has "Brachial Plexus" string in one
-        # or more fields
+        # She types in input search box:
+        # "Brachial Plexus Experiment" and wait to see the results.
+        searchbox.send_keys('Brachial Plexus Experiment')
+        
+        # Then she hits enter, and wait for results
+        searchbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # We consider, by now, that exists a row with one field containing
+        # the string "Brachial Plexus Experiment"
+        table = self.browser.find_element_by_id('id_results_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            any(row.text == 'Brachial Plexus Experiment' for row in rows)
+        )
+
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
