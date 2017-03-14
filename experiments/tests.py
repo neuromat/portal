a83cedc8a147a80f1ce23f7fb468ae39813a515b
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-14 16:11:44 -0300

    Basic model for Experiments and associated migrations

--- experiments/tests.py
+++ experiments/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from experiments.models import Experiment
 
 
 class HomePageTest(TestCase):
@@ -6,3 +7,10 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'experiments/home.html')
+
+
+class ExperimentModelTest(TestCase):
+
+    def test_default_title(self):
+        experiment = Experiment()
+        self.assertEqual(experiment.title, '')
