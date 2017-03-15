6303bd8fd207274af5c907c3c335ebdc30952c77
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-15 12:44:58 -0300

    Split out unit tests into two files

--- experiments/tests/test_models.py
+++ experiments/tests/test_models.py
@@ -4,13 +4,6 @@ from experiments.models import Experiment, Study
 from datetime import datetime
 
 
-class HomePageTest(TestCase):
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'experiments/home.html')
-
-
 class ExperimentModelTest(TestCase):
 
     def test_default_attributes(self):
