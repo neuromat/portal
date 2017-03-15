238a0e1a363f2a076b641b4a7562374743fec82d
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-15 11:04:59 -0300

    Tests for new attributes, cannot save title and description empty attributes, new migrations

--- experiments/models.py
+++ experiments/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Experiment(models.Model):
     title = models.CharField(max_length=150, default='')
+    description = models.TextField(default='')
+    is_public = models.BooleanField(default=False)
+    data_acquisition_done = models.BooleanField(default=False)
--- experiments/tests.py
+++ experiments/tests.py
@@ -12,12 +12,16 @@ class HomePageTest(TestCase):
 
 class ExperimentModelTest(TestCase):
 
-    def test_default_title(self):
+    def test_default_attributes(self):
         experiment = Experiment()
         self.assertEqual(experiment.title, '')
+        self.assertEqual(experiment.description, '')
+        self.assertEqual(experiment.is_public, False)
+        self.assertEqual(experiment.data_acquisition_done, False)
 
-    def test_cannot_save_empty_title(self):
-        experiment = Experiment(title='')
+    def test_cannot_save_empty_attributes(self):
+        # TODO: is it necessary to test for one attribute at time too?
+        experiment = Experiment(title='', description='')
         with self.assertRaises(ValidationError):
             experiment.save()
             experiment.full_clean()
