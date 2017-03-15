da4e21a51c19f6340409fc346d4b910ed2401ca8
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-15 12:35:57 -0300

    Add Study Model test, Study model, foreign key relationship test, and make migrations

--- experiments/models.py
+++ experiments/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
+class Study(models.Model):
+    title = models.CharField(max_length=150, default='')
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+
+
 class Experiment(models.Model):
     title = models.CharField(max_length=150, default='')
     description = models.TextField(default='')
     is_public = models.BooleanField(default=False)
     data_acquisition_done = models.BooleanField(default=False)
+    study = models.ForeignKey(Study, default='')
+
--- experiments/tests.py
+++ experiments/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from experiments.models import Experiment
+from experiments.models import Experiment, Study
+from datetime import datetime
 
 
 class HomePageTest(TestCase):
@@ -21,7 +22,32 @@ class ExperimentModelTest(TestCase):
 
     def test_cannot_save_empty_attributes(self):
         # TODO: is it necessary to test for one attribute at time too?
-        experiment = Experiment(title='', description='')
+        study = Study.objects.create(start_date=datetime.utcnow())
+        experiment = Experiment(title='', description='', study=study)
         with self.assertRaises(ValidationError):
             experiment.save()
             experiment.full_clean()
+
+    def test_experiment_is_related_to_study(self):
+        study = Study.objects.create(start_date=datetime.utcnow())
+        experiment = Experiment()
+        experiment.study = study
+        experiment.save()
+        self.assertIn(experiment, study.experiment_set.all())
+
+
+class StudyModelTest(TestCase):
+
+    def test_default_attributes(self):
+        study = Study()
+        self.assertEqual(study.title, '')
+        self.assertEqual(study.description, '')
+        self.assertEqual(study.start_date, None)
+        self.assertEqual(study.end_date, None)
+
+    def test_cannot_save_empty_attributes(self):
+        # TODO: is it necessary to test for one attribute at time too?
+        study = Study(title='', description='', start_date='')
+        with self.assertRaises(ValidationError):
+            study.save()
+            study.full_clean()
