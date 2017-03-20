91332937d832520853b7334727d6db500791385b
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-20 08:59:20 -0300

    Just rearrange api.py to put API Views below API Serializers

--- experiments/api.py
+++ experiments/api.py
@@ -1,35 +1,9 @@
 from django.http import HttpResponse
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework import serializers
 import json
 
 from experiments.models import Experiment, Study, User, Researcher
-
-
-# API Views
-def experiment(request):
-    if request.method == 'POST':
-        study = Study.objects.get(id=request.POST['study'])
-        user = User.objects.get(id=request.POST['user'])
-        Experiment.objects.create(
-            title=request.POST['title'],
-            description=request.POST['description'],
-            study=study,
-            user=user
-        )
-        return HttpResponse(status=status.HTTP_201_CREATED)
-    experiment_dicts = [
-        {'id': experiment.id, 'title': experiment.title, 'description':
-            experiment.description, 'data_acquisition_done':
-            experiment.data_acquisition_done, 'study': experiment.study.id,
-         'user': experiment.user.id}
-        for experiment in Experiment.objects.all()
-        ]
-    return HttpResponse(
-        json.dumps(experiment_dicts),
-        content_type='application/json'
-    )
 
 
 # API Serializers
@@ -73,3 +47,28 @@ class ResearcherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Researcher
         fields = ('id', 'first_name', 'surname', 'email', 'studies')
+
+
+# API Views
+def experiment(request):
+    if request.method == 'POST':
+        study = Study.objects.get(id=request.POST['study'])
+        user = User.objects.get(id=request.POST['user'])
+        Experiment.objects.create(
+            title=request.POST['title'],
+            description=request.POST['description'],
+            study=study,
+            user=user
+        )
+        return HttpResponse(status=status.HTTP_201_CREATED)
+    experiment_dicts = [
+        {'id': experiment.id, 'title': experiment.title, 'description':
+            experiment.description, 'data_acquisition_done':
+            experiment.data_acquisition_done, 'study': experiment.study.id,
+         'user': experiment.user.id}
+        for experiment in Experiment.objects.all()
+        ]
+    return HttpResponse(
+        json.dumps(experiment_dicts),
+        content_type='application/json'
+    )
