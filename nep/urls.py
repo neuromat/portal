edc9764481661554ab10b0de75e2ff9b5c3a9cf4
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-03-23 16:39:48 -0300

    Adds admin url

--- nep/urls.py
+++ nep/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from experiments import views
 from experiments import api_urls
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^api/', include(api_urls))
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api_urls)),
 ]
