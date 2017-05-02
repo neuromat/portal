3ad827b80e2974774d858a976fc0af538d21e894
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-05-02 14:05:35 -0300

    Refactor test_models

--- experiments/tests/test_models.py
+++ experiments/tests/test_models.py
@@ -6,6 +6,27 @@ from datetime import datetime
 from experiments.models import Experiment, Study, Researcher, ProtocolComponent
 
 
+def create_researcher(nes_id, username):
+    owner = User.objects.create_user(username=username)
+    return Researcher.objects.create(nes_id=nes_id, owner=owner)
+
+
+def create_study(nes_id):
+    researcher = create_researcher(nes_id=nes_id, username='lab1')
+    return Study.objects.create(nes_id=nes_id, start_date=datetime.utcnow(),
+                                researcher=researcher, owner=researcher.owner)
+
+
+def create_experiment(nes_id):
+    """
+    Creates experiment model object that are used
+    in test classes that depends on it. 
+    """
+    study = create_study(nes_id=1)
+    return Experiment.objects.create(nes_id=nes_id, study=study,
+                                     owner=study.owner)
+
+
 class ResearcherModelTest(TestCase):
 
     def test_default_attributes(self):
@@ -21,11 +42,10 @@ class ResearcherModelTest(TestCase):
             researcher.full_clean()
 
     def test_duplicate_researchers_are_invalid(self):
-        owner = User.objects.create_user(username='lab1')
-        Researcher.objects.create(nes_id=1, owner=owner)
-        researcher = Researcher(nes_id=1, owner=owner)
+        researcher1 = create_researcher(nes_id=1, username='lab1')
+        researcher2 = Researcher(nes_id=1, owner=researcher1.owner)
         with self.assertRaises(ValidationError):
-            researcher.full_clean()
+            researcher2.full_clean()
 
     def test_CAN_save_same_researcher_to_different_owners(self):
         owner1 = User.objects.create_user(username='lab1')
@@ -69,16 +89,14 @@ class StudyModelTest(TestCase):
             study.full_clean()
 
     def test_CAN_save_same_study_to_different_owners(self):
-        owner1 = User.objects.create_user(username='lab1')
-        owner2 = User.objects.create_user(username='lab2')
-        researcher1 = Researcher.objects.create(nes_id=1, owner=owner1)
-        researcher2 = Researcher.objects.create(nes_id=1, owner=owner2)
+        researcher1 = create_researcher(nes_id=1, username='lab1')
+        researcher2 = create_researcher(nes_id=1, username='lab2')
         Study.objects.create(nes_id=1, start_date=datetime.utcnow(),
-                             researcher=researcher1, owner=owner1)
+                             researcher=researcher1, owner=researcher1.owner)
         study = Study(title='A title', description='A description', nes_id=1,
                       start_date=datetime.utcnow(),
-                      end_date=datetime.utcnow(), 
-                      researcher=researcher2, owner=owner2)
+                      end_date=datetime.utcnow(),
+                      researcher=researcher2, owner=researcher2.owner)
         study.full_clean()
 
     def test_study_is_related_to_researcher_and_owner(self):
@@ -127,17 +145,19 @@ class ExperimentModelTest(TestCase):
             experiment.full_clean()
 
     def test_CAN_save_same_experiment_to_different_owners(self):
-        owner1 = User.objects.create_user(username='lab1')
-        owner2 = User.objects.create_user(username='lab2')
-        researcher1 = Researcher.objects.create(nes_id=1, owner=owner1)
-        researcher2 = Researcher.objects.create(nes_id=1, owner=owner2)
+        researcher1 = create_researcher(nes_id=1, username='lab1')
+        researcher2 = create_researcher(nes_id=1, username='lab2')
         study1 = Study.objects.create(nes_id=1, start_date=datetime.utcnow(),
-                                      researcher=researcher1, owner=owner1)
+                                      researcher=researcher1,
+                                      owner=researcher1.owner)
         study2 = Study.objects.create(nes_id=1, start_date=datetime.utcnow(),
-                                      researcher=researcher2, owner=owner2)
-        Experiment.objects.create(nes_id=1, study=study1, owner=owner1)
+                                      researcher=researcher2,
+                                      owner=researcher2.owner)
+        Experiment.objects.create(nes_id=1, study=study1,
+                                  owner=researcher1.owner)
         experiment = Experiment(title='A title', description='A description',
-                                nes_id=1, study=study2, owner=owner2)
+                                nes_id=1, study=study2,
+                                owner=researcher2.owner)
         experiment.full_clean()
 
 
@@ -166,23 +186,16 @@ class ProtocolComponentModelTest(TestCase):
         self.assertEqual(protocol_component.nes_id, None)
 
     def test_protocol_component_is_related_to_experiment_and_owner(self):
-        owner = User.objects.create_user(username='lab1')
-        researcher = Researcher.objects.create(nes_id=1, owner=owner)
-        study = Study.objects.create(
-            nes_id=1, start_date=datetime.utcnow(), researcher=researcher,
-            owner=owner)
-        experiment = Experiment.objects.create(
-            nes_id=1, title='A title', description='A description',
-            study=study, owner=owner
-        )
+        experiment = create_experiment(nes_id=1)
         protocolcomponent = ProtocolComponent(
             identification='An identification',
             component_type='A component type',
-            nes_id=1, experiment=experiment, owner=owner
+            nes_id=1, experiment=experiment, owner=experiment.owner
         )
         protocolcomponent.save()
         self.assertIn(protocolcomponent, experiment.protocol_components.all())
-        self.assertIn(protocolcomponent, owner.protocolcomponent_set.all())
+        self.assertIn(protocolcomponent,
+                      experiment.owner.protocolcomponent_set.all())
 
     # def test_cannot_save_empty_attributes(self):
     #     owner = User.objects.create_user(username='lab1')
