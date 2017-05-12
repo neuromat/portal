f8fb80753bfd7dd2d19cb5e972afa4d338976eda
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-05-12 14:16:55 -0300

    Makes raw header, content and footer

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -71,6 +71,9 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Neuroscience Experiments Database', header_text)
 
+        ##
+        # Tests for header
+        ##
         # She sees that in header bunner there is a search box invited her
         # to type terms/words that will be searched in the portal
         searchbox = self.browser.find_element_by_id('id_search_box')
@@ -79,16 +82,57 @@ class NewVisitorTest(LiveServerTestCase):
             'Type key terms/words to be searched'
         )
 
+        ##
+        # Tests for content
+        ##
         # As there are experiments sended to Portal, she sees the home
-        # page have a list of experiments
-        experiment = Experiment.objects.first()
+        # page have a list of experiments in a table.
+        # She reads in "List of Experiments" in the table title
+        table_title = self.browser.find_element_by_id(
+            'id_table_title').find_element_by_tag_name('h2').text
+        self.assertEqual('List of Experiments', table_title)
+        # She sees a list of experiments with columns: Title, Description
         table = self.browser.find_element_by_id('id_experiments_table')
-        rows = table.find_elements_by_tag_name('tr')
+        row_headers = table.find_element_by_tag_name(
+            'thead').find_element_by_tag_name('tr')
+        col_headers = row_headers.find_elements_by_tag_name('th')
+        self.assertTrue(col_headers[0].text == 'Title')
+        self.assertTrue(col_headers[1].text == 'Description')
+        # She sees the content of the list
+        experiment = Experiment.objects.first()
+        rows = table.find_element_by_tag_name(
+            'tbody').find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == experiment.title for row in rows),
-            f'Experiment does {experiment.title} not appear in experiment '
-            f'list. Content where:\n{table.text}'
+            any(row.find_elements_by_tag_name('td')[0].text ==
+                experiment.title for row in rows)
         )
+        self.assertTrue(
+            any(row.find_elements_by_tag_name('td')[1].text ==
+                experiment.description for row in rows)
+        )
+
+        ##
+        # Tests for footer
+        ##
+        # She notices that in the footer there is information
+        # about ways of contact institution.
+        footer_section = self.browser.find_element_by_id('id_footer_section')
+        footer_header_text = footer_section.find_element_by_tag_name('h4').text
+        self.assertIn('Contact', footer_header_text)
+        footer_content = footer_section.find_element_by_id(
+            'id_footer_content').text
+        self.assertIn('Address:', footer_content)
+        self.assertIn('Matão St., 1010 - Cidade Universitária - São Paulo - '
+                      'SP - Brasil. 05508-090. Veja o mapa', footer_content)
+        self.assertIn('Phone:', footer_content)
+        self.assertIn('+55 11 3091-1717', footer_content)
+        self.assertIn('Email:', footer_content)
+        self.assertIn('neuromat@numec.prp.usp.br', footer_content)
+        self.assertIn('Media contact:', footer_content)
+        self.assertIn('comunicacao@numec.prp.usp.br', footer_content)
+        footer_license = footer_section.find_element_by_id('id_license').text
+        self.assertIn('This site content is licensed with Creative Commons '
+                      'Attributions 3.0.', footer_license)
 
         self.fail('Finish the test!')
 
