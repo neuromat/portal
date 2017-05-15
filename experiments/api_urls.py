5770928355bfa5eb8ba730f65ea7d4da8dfaf766
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-05-15 17:03:33 -0300

    Adds schema view to API

--- experiments/api_urls.py
+++ experiments/api_urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
 from experiments import api
 
 # Create a router and register our viewsets with it.
@@ -14,6 +16,10 @@ router.register(r'protocol_components', api.ProtocolComponentViewSet,
 router.register(r'groups', api.GroupViewSet,
                 base_name='api_groups')
 
+# Get rest framework schema view
+schema_view = get_schema_view(title='NEP API')
+
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
 ]
