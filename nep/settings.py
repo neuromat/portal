3762c211d8a78ce1ca60dc4c9f86c8b9afc84f78
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-06-01 08:56:49 -0300

    Update settings

--- nep/settings.py
+++ nep/settings.py
@@ -15,15 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/#secret-key
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['']
