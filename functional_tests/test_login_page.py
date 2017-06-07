cdd25876ca3880fdd6252764e9ef7fa196287500
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-06-07 17:30:06 -0300

    Tests login page

--- functional_tests/test_login_page.py
+++ functional_tests/test_login_page.py
@@ -1,11 +1,11 @@
 import time
-
+from selenium.webdriver.common.keys import Keys
 from functional_tests.base import FunctionalTest
 
 
 class LoginPageTest(FunctionalTest):
 
-    def test_can_view_login_page(self):
+    def test_can_view_login_page_and_login_in_the_system(self):
         self.browser.get(self.live_server_url)
 
         # The visitor is in home page and see that is a "Log In" link in up
@@ -17,5 +17,22 @@ class LoginPageTest(FunctionalTest):
         # where she can enter her credentials.
         login_header = self.browser.find_element_by_tag_name('h3').text
         self.assertIn('Log In', login_header)
+
+        # She enters her credentials and click in login button
+        # Obs.: this id's isn't in template because the template form elements
+        # came from django auth system. You can see them by inspecting
+        # element in browser.
+        inputbox_username = self.browser.find_element_by_id('id_username')
+        inputbox_username.send_keys('claudia')
+        inputbox_password = self.browser.find_element_by_id('id_password')
+        inputbox_password.send_keys('passwd-claudia')
+
+        # When she hits enter to make login.
+        login_button = self.browser.find_element_by_id('id_submit')
+        login_button.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # She is redirected to home page, but now logged in. So, she sees a
+        # message of welcome and your in up right corner of the screen.
 
         self.fail('Finish this test!')
