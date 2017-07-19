f57c82d0ea149606de586496ac96bec8c9a48cd4
mruizolazar <margaritaruizolazar@gmail.com>
2017-07-19 15:09:21 -0300

    PRT-108 Download do objeto Experiment

--- downloads/models.py
+++ downloads/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+def get_export_dir(instance, filename):
+    return "export/%(user)s/%(export)s/%(filename)s" % \
+        {'user': instance.export.user.id,
+         'export': instance.export.pk, 'filename': filename}
+
+class Export(models.Model):
+    user = models.ForeignKey(User, null=False)
+    date = models.DateTimeField(null=False, auto_now_add=True)
+    input_file = models.FileField(upload_to=get_export_dir, null=False, max_length=1000)
+    output_export = models.FileField(upload_to=get_export_dir, null=False, max_length=1000)
+
+    def delete(self, *args, **kwargs):
+        self.content.delete()
+        super(Export, self).delete(*args, **kwargs)
--- downloads/views.py
+++ downloads/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Export
 
 
 # Create your views here.
+def create_export_instance(user):
+    export_instance = Export(user=user)
+
+    export_instance.save()
+
+    return export_instance
+
 def download_view(request):
     return HttpResponse("Download view")
     # description = ''
