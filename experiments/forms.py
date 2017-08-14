f98d18154689842af2449aea613d6cf1054f3a1c
caco <crns13@gmail.com>
2017-08-14 08:56:50 -0300

    Creates a django form inheriting from haystack SearchForm class. It substitutes the manually created form before

---
+++ experiments/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from haystack.forms import SearchForm
+
+
+class NepSearchForm(SearchForm):
+    q = forms.CharField(
+        required=True, label='',
+        widget=forms.TextInput(
+            attrs={'type': 'search',
+                   'placeholder': 'Type key terms/words to be searched',
+                   'class': 'search-box'}
+        )
+    )
+    filter = forms.ChoiceField(
+        required=False, label='',
+        choices=[
+            ('', 'Select one or more list itens to filter experiments '
+                 'that has:'),
+            ('eeg', 'EEG'), ('tms', 'TMS'), ('emg', 'EMG'),
+            ('goalkeeper', 'Goalkeeper game phase'),
+            ('cinematic', 'Cinematic measures'),
+            ('stabilometry', 'Stabilometry'), ('answertime', 'Answer time'),
+            ('psychophysical', 'Psychophysical measures'),
+            ('verbal', 'Verbal answer'),
+            ('psychometric', 'Psychometric scales'),
+            ('unitary', 'Unitary register')
+        ],
+        widget=forms.Select(
+            attrs={'class': 'selectpicker search-select'}
+        )
+    )
+
+    def search(self):
+        # First, store the SearchQuerySet received from other processing.
+        sqs = super(NepSearchForm, self).search()
+
+        if not self.is_valid():
+            return self.no_query_found()
+
+        return sqs
