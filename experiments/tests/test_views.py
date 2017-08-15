2bd7b89338f3cde88ab0a6a2c8a578e6f7180941
caco <crns13@gmail.com>
2017-08-15 08:55:38 -0300

    Fixes view test to include filter field in search

--- experiments/tests/test_views.py
+++ experiments/tests/test_views.py
@@ -161,12 +161,13 @@ class SearchTest(TestCase):
     def setUp(self):
         global_setup_ut()
 
-    # def test_search_redirects_to_homepage_with_search_results(self):
-    #     response = self.client.post('/search/')
-    #     self.assertEqual(response.status_code, 200)
-    #     # TODO: is it needed to test for redirected page?
+    def test_search_redirects_to_homepage_with_search_results(self):
+        response = self.client.post('/search/')
+        self.assertEqual(response.status_code, 200)
+        # TODO: is it needed to test for redirected page?
 
     def test_search_returns_only_approved_experiments(self):
-        response = self.client.get('/search/', {'q': 'Braquial+Plexus'})
-        # TODO
+        response = self.client.get('/search/', {'q': 'Braquial+Plexus',
+                                                'filter': ''})
+        # TODO: complete this test!
 
