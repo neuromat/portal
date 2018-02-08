3c7cfe2426f96d6d6147d033d63db2eb937b5b77
caco <crns13@gmail.com>
2018-02-08 14:57:51 -0200

    Translates javascript and other missing strings

--- static/js/download.js
+++ static/js/download.js
@@ -1,19 +1,6 @@
 /**
  * Created by mruizo on 01/08/17.
  */
-// $(function () {
-//     var experimentId = $("#experimentId").val();
-//    $(document).on("click", "a.fileDownloadSimpleRichExperience", function () {
-//        $.fileDownload($(this).prop('href'), {
-//            preparingMessageHtml: "We are preparing your download, please wait...",
-//            failMessageHtml: "There was a problem generating your download, please try again."
-//        }).done(function () {
-//            window.location = '/experiments/' + experimentId;
-//        });
-//
-//        return false;
-//    });
-// });
 
 $(function () {
     var experimentId =  $("#experimentId").val();
@@ -39,85 +26,3 @@ $(function () {
         return false;
     });
 });
-
-// $(function() {
-//     var progressTimer,
-//         experimentId = $("#experimentId").val(),
-//         progressbar = $("#progressbar"),
-//         progressLabel = $( ".progress-label" ),
-//         dialogButtons = [{
-//             text: "Cancel Download",
-//             click: closeDownload
-//         }],
-//         dialog = $( "#dialog" ).dialog({
-//             autoOpen: false,
-//             closeOnEscape: false,
-//             resizable: false,
-//             buttons: dialogButtons,
-//             open: function() {
-//                 // progressTimer = setTimeout( progress, 2000 );
-//                 downloadFile();
-//             },
-//             beforeClose: function() {
-//                 downloadButton.button( "option", {
-//                     disabled: false,
-//                     label: "Start Download"
-//                 });
-//             }
-//         }),
-//         downloadButton = $( "#downloadButton" ).button().on( "click", function() {
-//             $( this ).button( "option", {
-//                 disabled: true,
-//                 label: "Downloading..."
-//             });
-//             dialog.dialog( "open" );
-//
-//         });
-//
-//     progressbar.progressbar({
-//         value: false,
-//         change: function () {
-//             progressLabel.text("Current Progress: " + progressbar.progressbar("value") + "%");
-//         },
-//         complete: function () {
-//             progressLabel.text("Complete!");
-//             dialog.dialog("option", "buttons", [{
-//                 text: "Close",
-//                 click: closeDownload
-//             }]);
-//             $(".ui-dialog button").last().trigger("focus");
-//         }
-//     });
-//
-//     function progress() {
-//         var val = progressbar.progressbar("value") || 0;
-//         progressbar.progressbar("value", val + Math.floor( Math.random() * 3 ));
-//         if(val <= 99){
-//             progressTimer = setTimeout( progress, 50 );
-//         }
-//     }
-//
-//     function closeDownload() {
-//         clearTimeout( progressTimer );
-//         dialog
-//             .dialog( "option", "buttons", dialogButtons )
-//             .dialog( "close" );
-//         progressbar.progressbar( "value", false );
-//         progressLabel
-//             .text( "Starting download..." );
-//         downloadButton.trigger( "focus" );
-//     }
-//
-//     function downloadFile() {
-//         experimentId = $("#experimentId").val()
-//         $.ajax({
-//                 type: "POST",
-//                 url: "/downloads/7/",
-//                 success: function(data) {
-//                     dialog.dialog( "close" );
-//                 }
-//             });
-//         // dialog.dialog("close");
-//     }
-//
-// });
