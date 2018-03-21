ccc06eb0ba865985861e94ead3d89449ad9228cd
caco <crns13@gmail.com>
2018-03-21 09:12:45 -0300

    Test for age null in csv file in Participants.csv

--- downloads/tests/test_views.py
+++ downloads/tests/test_views.py
@@ -19,6 +19,10 @@ TEMP_MEDIA_ROOT = os.path.join(tempfile.mkdtemp())
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class DownloadCreateView(TestCase):
 
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(TEMP_MEDIA_ROOT)
+
     @staticmethod
     def create_basic_experiment_data():
         owner = User.objects.create_user(username='lab1', password='nep-lab1')
@@ -26,19 +30,13 @@ class DownloadCreateView(TestCase):
         create_study(1, experiment)
         return experiment
 
-    def test_create_download_subdir_if_not_exist(self):
-        experiment = self.create_basic_experiment_data()
-        download_create(experiment.id, '')
-
-        self.assertTrue(
-            os.path.exists(os.path.join(TEMP_MEDIA_ROOT, 'download'))
-        )
-
-    def test_do_not_write_age_column_in_csv_file_if_participants_has_date_null_1(self):
-        # example file:
-        # /media/download/1/Group_odit/Per_participant_data/Participant_312
-        # /STEP_861-20-7671_QUESTIONNAIRE/Q5489_narakas-and-waikakul.csv
-        experiment = self.create_basic_experiment_data()
+    @staticmethod
+    def create_questionnaire(experiment):
+        """
+        Necessary the following files to exist:
+            - settings.BASE_DIR/experiments/tests/questionnaire7.csv
+            - settings.BASE_DIR/experiments/tests/response_questionnaire7.json
+        """
         group = create_group(1, experiment)
         participant = create_participant(1, group)
         participant.age = None
@@ -54,6 +52,23 @@ class DownloadCreateView(TestCase):
             settings.BASE_DIR +
             '/experiments/tests/response_questionnaire7.json'
         )
+        return group
+
+    def test_create_download_subdir_if_not_exist(self):
+        experiment = self.create_basic_experiment_data()
+        download_create(experiment.id, '')
+
+        self.assertTrue(
+            os.path.exists(os.path.join(TEMP_MEDIA_ROOT, 'download'))
+        )
+
+    def test_do_not_write_age_column_in_csv_file_if_participants_has_date_null_1(self):
+        # example file:
+        # /media/download/1/Group_odit/Per_participant_data/Participant_312
+        # /STEP_861-20-7671_QUESTIONNAIRE/Q5489_narakas-and-waikakul.csv
+        experiment = self.create_basic_experiment_data()
+        group = self.create_questionnaire(experiment)
+
         download_create(experiment.id, '')
 
         # check in Per_participant_data subdir
@@ -66,5 +81,18 @@ class DownloadCreateView(TestCase):
                 f = open(os.path.join(root, files[0]), 'r')
                 self.assertNotIn('age (years)', f.read())
 
-        # clear temp dir
-        shutil.rmtree(TEMP_MEDIA_ROOT)
+    def test_do_not_write_age_column_in_csv_file_if_participants_has_date_null_2(self):
+        # example file:
+        # /media/download/1/Group_odit/Participants.csv
+        experiment = self.create_basic_experiment_data()
+        group = self.create_questionnaire(experiment)
+
+        download_create(experiment.id, '')
+
+        # check in Particpants.csv file
+        participants_file = os.path.join(
+            TEMP_MEDIA_ROOT, 'download', str(experiment.id),
+            'Group_' + slugify(group.title), 'Participants.csv'
+        )
+        f = open(participants_file, 'r')
+        self.assertNotIn('age (years)', f.read())
